b02ab8795660e4e4c5310cb79764fe884a905631
Rohan <rohan.gangaraju@gmail.com>
2018-04-28 12:13:47 -0400

    Optimize writing statistics

--- flaskserver.py
+++ flaskserver.py
@@ -1,4 +1,4 @@
-from flask import Flask
+rom flask import Flask
 from flask import request
 import ssl
 import base64
@@ -11,18 +11,30 @@ from time import gmtime, strftime
 
 app = Flask(__name__, static_folder='build')
 
+user_count = 0
+user_list = []
+WRITE_THRESHOLD = 5
+
 # Serve React App
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve(path):
+    global user_count
+    global user_list
+    global WRITE_THRESHOLD
     timestamp = strftime("%Y-%m-%d %H:%M:%S", gmtime())
     ip = request.remote_addr
-    print("Request IP ", ip, " Time ", timestamp)
-    try:
-        with open("user_log.csv","a") as f:
-            f.write(ip+','+timestamp+'\n')
-    except Exception as e:
-        print("type error: " + str(e))
+    print("User_count ", user_count, "Request IP ", ip, " Time ", timestamp)
+    user_list.append(str(user_count)+','+ip+','+timestamp)
+    user_count += 1
+    if user_count%WRITE_THRESHOLD==0:
+        try:
+            print("writing user data")
+            with open("user_log.csv","a") as f:
+                f.write("\n"+"\n".join(user_list))
+                user_list = []
+        except Exception as e:
+            print("type error: " + str(e))
     if path != "" and os.path.exists("build/" + path):
         return send_from_directory('build/', path)
     else:
